2e26cbc1a85605b28e18c486225901cdcc242ef7
CKNLan <caokhacngoclan@gmail.com>
2022-08-24 23:29:28 +0700

    Add .ipynb file; preprocessing address for querying

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-'''
-A Web application that shows Google Maps around schools, using
-the Flask framework, and the Google Maps API.
-'''
-
-from flask import Flask, render_template, abort
+from dataclasses import replace
+from json import dumps
+from flask import Flask, render_template
 import requests
 from dotenv import load_dotenv
 import os
@@ -11,21 +8,27 @@ import os
 # from OSMPythonTools.api import Api
 # api = Api()
 # way = api.query('way/5887599')
-from OSMPythonTools.overpass import Overpass
-overpass = Overpass()
+# from OSMPythonTools.overpass import Overpass
+# overpass = Overpass()
 load_dotenv()
-
 
 app = Flask(__name__)
 
+def preprocessAddress(address = ""):
+    return address.replace(" ", "+").replace("and", "%26")
+
 @app.route("/")
 def index():
-    result = overpass.query('way["name"="20th and Folsom Streets"]; out body;')
-    print(result)
-    # stephansdom = result.elements()[0]
-    # print(stephansdom.tag('name:en'))
-    
-    return render_template('index.html',
+    # requests.get()
+    print(os.getenv("SEARCH_URL").format(preprocessAddress("20th and Folsom Streets")))
+
+    return render_template('index.html'
+        , sf_coor = os.getenv("SF_COORDINATE")
+        , title_layer_url_template = os.getenv("TITLE_LAYER_URL_TEMPLATE")
+        , title_layer_options = dumps({
+            'maxZoom': 50,
+            'attribution': '© OpenStreetMap'
+        })
     )
     # return render_template('index.html', schools=schools)
 
--- static/index.js
+++ static/index.js
@@ -1,6 +1,15 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+function initMap(sf_coor, title_layer_url_template, title_layer_options) {
+    var map = L.map('map').setView(sf_coor, 12);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap'
-}).addTo(map);
+    L.tileLayer(title_layer_url_template, JSON.parse(title_layer_options)).addTo(map);
+
+    // var marker =
+    L.marker([37.769368099999994, -122.48218371117709])
+        .addTo(map);
+
+    // address = "20th and Folsom Streets"
+    // uri = `${address}`
+    // $.get(location.protocol + '//nominatim.openstreetmap.org/search?format=json&q=' + address, function (data) {
+    //     console.log(data);
+    // });
+}
