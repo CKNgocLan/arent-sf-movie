d23d3ff65c3a2c17985798f022b9ede4ba52ced5
CKNLan <caokhacngoclan@gmail.com>
2022-08-24 12:39:43 +0700

    Change use of Google API to OpenStreetMap

--- app.py
+++ app.py
@@ -4,49 +4,30 @@ the Flask framework, and the Google Maps API.
 '''
 
 from flask import Flask, render_template, abort
-# import request
+import requests
 from dotenv import load_dotenv
 import os
+
+# from OSMPythonTools.api import Api
+# api = Api()
+# way = api.query('way/5887599')
+from OSMPythonTools.overpass import Overpass
+overpass = Overpass()
 load_dotenv()
 
 
 app = Flask(__name__)
 
-class School:
-    def __init__(self, key, name, lat, lng):
-        self.key  = key
-        self.name = name
-        self.lat  = lat
-        self.lng  = lng
-
-schools = (
-    School('hv',      'Happy Valley Elementary',   37.9045286, -122.1445772),
-    School('stanley', 'Stanley Middle',            37.8884474, -122.1155922),
-    School('wci',     'Walnut Creek Intermediate', 37.9093673, -122.0580063)
-)
-schools_by_key = {school.key: school for school in schools}
-
-nearby_search_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&keyword=cruise&key=" + os.getenv("API_KEY")
-
-
-
 @app.route("/")
 def index():
-    school = schools_by_key.get('hv')
+    result = overpass.query('way["name"="20th and Folsom Streets"]; out body;')
+    print(result)
+    # stephansdom = result.elements()[0]
+    # print(stephansdom.tag('name:en'))
+    
     return render_template('index.html',
-        school = school,
-        map_api = "https://maps.googleapis.com/maps/api/js?key={}-L0&callback=initMap".format(os.getenv("API_KEY"))
     )
     # return render_template('index.html', schools=schools)
 
-
-@app.route("/<school_code>")
-def show_school(school_code):
-    # school = schools_by_key.get(school_code)
-    school = schools_by_key.get('hv')
-    if school:
-        return render_template('index.html', school=school)
-    else:
-        abort(404)
-
-# app.run(host='localhost', debug=True)
+if __name__ == '__main__':
+    app.run(debug = True)
